d4a9c0b598b1496a18fa8d3b01c0b5427b5042ad
Kayleigh N <kayleigh.kasitra@gmail.com>
2026-01-27 10:27:53 +0200

    Reverting to older site design

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
-function setAge() {
-  var today = new Date();
-  var birthDate = new Date("2002-08-04");
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
+function loadNavBar() {
+  var navbar = document.getElementById("navigation-bar");
+  if (navbar.style.display === "none") {
+    navbar.style.display = "flex";
+  } else {
+    navbar.style.display = "none";
   }
-
-  document.getElementById("age-string").innerHTML = age;
-}
-
-function setLocalTime() {
-  const t_element = document.getElementById("timezone");
-
-  if (!t_element) return;
-
-  const now = new Date();
-  const options = {
-    hour: "2-digit",
-    minute: "2-digit",
-    timeZone: "Africa/Johannesburg",
-  };
-  const timeFormatted = now.toLocaleTimeString([], options);
-
-  t_element.textContent = timeFormatted;
 }
 
 function loadImages() {
-  var contentSection = document.getElementById("gallery-content");
+  var contentSection = document.getElementById("gallery-content-section");
   const count = 35;
   for (i = 0; i < count; i++) {
-    var path = `../gallery/images/${i}.png`;
+    var path = `../images/gallery/${i}.png`;
     let image = document.createElement("img");
     image.src = path;
     image.draggable = false;
-    image.style.objectFit = "cover";
     image.setAttribute("onclick", `previewImage(${i})`);
     contentSection.appendChild(image);
   }
 }
 
 function previewImage(id) {
-  var path = `../gallery/images/${id}.png`;
+  var path = `../images/gallery/${id}.png`;
   var overlayImg = document.getElementById("image-preview");
   overlayImg.src = path;
   document.getElementById("gallery-overlay").style.display = "flex";
@@ -50,26 +30,3 @@ function previewImage(id) {
 function hideOverlay() {
   document.getElementById("gallery-overlay").style.display = "none";
 }
-
-function displayGameOverlay(id) {
-  fetch("info.json")
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("game-title-slot").innerText = data[id]["title"];
-      document.getElementById("game-image-slot-1").src = data[id]["image-1"];
-      document.getElementById("game-description-slot-1").innerText =
-        data[id]["info-1"];
-      document.getElementById("game-image-slot-2").src = data[id]["image-2"];
-      document.getElementById("game-description-slot-2").innerText =
-        data[id]["info-2"];
-    })
-    .catch((error) => console.error("Unable to load game information."));
-  document.getElementById("game-info-container").style.display = "flex";
-}
-
-function hideGameOverlay() {
-  document.getElementById("game-info-container").style.display = "none";
-}
-
-setLocalTime();
-setInterval(setLocalTime, 1000);
